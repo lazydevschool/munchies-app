8b601eb28c26e06a00ba634589518678b6e730cf
lazydevschooltut <182364505+lazydevschooltut@users.noreply.github.com>
2024-10-26 16:22:28 -0600

    show auth store on debug page for sanity check

--- client/src/routes/debug/+page.svelte
+++ client/src/routes/debug/+page.svelte
@@ -1,7 +1,13 @@
 <!-- src/routes/debug/+page.svelte -->
 <script lang="ts">
+  import { authStore } from '$lib/stores/authStore';
   export let data: { debugData: any };
+
+
 </script>
 
 <h1>Debug Information</h1>
 <pre>{JSON.stringify(data.debugData, null, 2)}</pre>
+<hr>
+<h2>Auth Store</h2>
+<pre>{JSON.stringify({authStore:$authStore}, null, 2)}</pre>
